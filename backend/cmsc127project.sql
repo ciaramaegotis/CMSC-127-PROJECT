a986448c513639dce1f1e0329232507e49e01654
Ciara Mae Gotis <crgotis@up.edu.ph>
2017-11-27 19:43:43 +0800

    fixed bug in database

--- backend/cmsc127project.sql
+++ backend/cmsc127project.sql
@@ -34,7 +34,7 @@ CREATE table TRANSACTION(
 		KEY(Transaction_number),
 	CONSTRAINT product_card_number_fk FOREIGN KEY(Card_number)
 		REFERENCES CUSTOMER(Card_number),
-	CONSTRAINT product_branch_id_fk FOREIGN KEY(Branch_id)
+	CONSTRAINT transaction_branch_id_fk FOREIGN KEY(Branch_id)
 		REFERENCES BRANCH(Branch_id)
 );
 
@@ -44,8 +44,7 @@ CREATE table PRODUCT(
 	Product_price float(10, 5),
 	Branch_id int(10),
 	CONSTRAINT product_product_number_pk PRIMARY KEY(Product_number),
-	CONSTRAINT product_branch_id_fk FOREIGN KEY(Branch_id)
-	REFERENCES BRANCH(Branch_id)
+	CONSTRAINT product_branch_id_fk FOREIGN KEY(Branch_id) REFERENCES BRANCH(Branch_id)
 );
 
 CREATE table PROMOSTAR(
@@ -56,26 +55,21 @@ CREATE table PROMOSTAR(
 	Card_number int(10),
 	CONSTRAINT promostar_control_number_pk PRIMARY KEY(Control_number),
 	CONSTRAINT promostar_product_number_fk FOREIGN KEY(Product_number) REFERENCES PRODUCT(Product_number),
-	CONSTRAINT promostar_card_number_fk FOREIGN KEY(Card_number)
-		REFERENCES CUSTOMER(Card_number)
+	CONSTRAINT promostar_card_number_fk FOREIGN KEY(Card_number) REFERENCES CUSTOMER(Card_number)
 );
 
 CREATE table PRODUCT_SPENT_PROMOSTAR(
 	Transaction_number int(10),
 	Control_number int(10),
-	CONSTRAINT product_spent_promostar_transaction_number_pk PRIMARY 
-		KEY(Transaction_number),
-	CONSTRAINT product_spent_promostar_control_number_fk FOREIGN 
-		KEY(Control_number) REFERENCES PROMOSTAR(Control_number)
+	CONSTRAINT product_spent_promostar_transaction_number_pk PRIMARY KEY(Transaction_number),
+	CONSTRAINT product_spent_promostar_control_number_fk FOREIGN KEY(Control_number) REFERENCES PROMOSTAR(Control_number)
 );
 
 CREATE table PRODUCT_ACCUMULATED_PROMOSTAR(
 	Transaction_number int(10),
 	Control_number int(10),
-	CONSTRAINT product_accumulated_promostar_transaction_number_pk PRIMARY 
-		KEY(Transaction_number),
-	CONSTRAINT product_accumulated_promostar_control_number_fk FOREIGN 
-		KEY(Control_number) REFERENCES PROMOSTAR(Control_number)
+	CONSTRAINT product_accumulated_promostar_transaction_number_pk PRIMARY KEY(Transaction_number),
+	CONSTRAINT product_accumulated_promostar_control_number_fk FOREIGN KEY(Control_number) REFERENCES PROMOSTAR(Control_number)
 );
 
 CREATE table STOCK(
@@ -84,8 +78,4 @@ CREATE table STOCK(
 	Stock int(10),
 	CONSTRAINT stock_branch_id_pk PRIMARY KEY(Branch_id, Product_name)
 );
-
-
-
-
 
